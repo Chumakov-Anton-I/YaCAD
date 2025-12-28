2ad8bb4aed91c561590d943b5f1e26c725060207
Anton-I-Chumakov <anton.chumakov92@gmail.com>
2025-12-29 00:00:22 +0300

    Add objects' definition file

---
+++ src/definitions.h
@@ -0,0 +1,11 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <QGraphicsItem>
+
+enum ItemType {
+    DotPointType = QGraphicsItem::UserType + 1,
+    LineSegmentType
+};
+
+#endif // DEFINITIONS_H
